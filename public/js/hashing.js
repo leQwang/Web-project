a5b77df9eff375c136019c8fac98591f76ce9834
leQwang <s3912105@rmit.edu.vn>
2023-05-20 11:24:36 +0700

    add acknowledgement

--- public/js/hashing.js
+++ public/js/hashing.js
@@ -1,4 +1,16 @@
-//https://www.makeuseof.com/nodejs-bcrypt-hash-verify-salt-password/
+/* RMIT University Vietnam
+Course: COSC2430 Web Programming
+Semester: 2023A
+Assessment: Assignment 2
+Author/ID: 
+Celina Vangstrup s3993395
+Doan Tran Thien Phuc s3926377
+Le Duy Quang s3912105
+Gustav Joachim Elbroend s3995055
+Damien Vincent Voelker s3995378
+Acknowledgement: RMIT Lecture Slide, https://www.makeuseof.com/nodejs-bcrypt-hash-verify-salt-password/*/
+
+
 const bcrypt = require('bcrypt');
 
 const hashPassword = async (password) => {
